b8bea3a7b9fc14461c2d1141acc3df13fd01fee4
Dirac <daronmitchel@gmail.com>
2025-04-11 21:51:51 -0500

    Upddate trivia.py Implemented run_quiz() function
    Implemented run_quiz() function in trivia.py to run the quiz game.
    Also added a exception handling for invalid input for Question class in
    correct_answer_index parameter.

--- trivia.py
+++ trivia.py
@@ -12,6 +12,9 @@ class Question:
         """
         self._question_text = question_text
         self._answer_options = answer_options
+        
+        if not 0 <= correct_answer_index < len(answer_options):
+            raise ValueError(f"El índice {correct_answer_index} esta fuera del rango de respuestas.")
         self._correct_answer_index = correct_answer_index
         
     
@@ -86,3 +89,49 @@ class Quiz:
         
         self._incorrect_answers += 1
         return False
+    
+    
+def run_quiz():
+
+    print("Bienvenido al juego de Trivia!")
+    print("Responde las preguntas eligiendo el número de la opción correcta.\n")
+
+    quiz = Quiz()
+    
+    question1 = Question("¿Quién escribió 'Cien años de soledad'?", ["Pablo Neruda", "Gabriel García Márquez", "Mario Vargas Llosa", "Julio Cortázar"], 1)
+    question2 = Question("¿Cuál es el resultado de 3² + 4²?", ["25", "12", "5", "49"], 0)
+    question3 = Question("¿En qué año ocurrió la Revolución Francesa?", ["1789", "1810", "1492", "1804"], 0)
+    question4 = Question("¿Cuál es el símbolo químico del oro?", ["Ag", "Au", "Gd", "Ga"], 1)
+    question5 = Question("¿Qué país tiene mayor población?", ["Estados Unidos", "India", "Rusia", "China"], 3)
+    question6 = Question("¿Qué tipo de energía produce una planta nuclear?", ["Eólica", "Solar", "Térmica", "Nuclear"], 3)
+    question7 = Question("¿Quién propuso la teoría de la relatividad?", ["Newton", "Einstein", "Tesla", "Bohr"], 1)
+    question8 = Question("¿Quién es el creador de Linux?", ["Bill Gates", "Richard Stallman", "Linus Torvalds", "Guido van Rossum "], 2)
+    question9 = Question("¿Cuál es el planeta más grande del sistema solar?", ["Tierra", "Júpiter", "Saturno", "Marte"], 1)
+    question10 = Question("¿Cuántos huesos tiene el cuerpo humano adulto?", ["206", "201", "210", "205"], 0)
+
+
+    quiz.add_question(question1)
+    quiz.add_question(question2)
+    quiz.add_question(question3)
+    quiz.add_question(question4)
+    quiz.add_question(question5)
+    quiz.add_question(question6)
+    quiz.add_question(question7)
+    quiz.add_question(question8)
+    quiz.add_question(question9)
+    quiz.add_question(question10)
+    
+    while quiz._current_question_index < len(quiz._questions):
+        current_question = quiz.get_next_question()
+        current_question.show_question()
+        
+        user_answer = int(input("Elige una opción: "))
+        quiz.answer_question(user_answer)
+        
+    print("\nJuego terminado. Aquí está tu puntuación:")
+    print(f"Preguntas contestadas: {len(quiz._questions)}")
+    print(f"Respuestas correctas: {quiz._correct_answers}")
+    print(f"Respuesta incorrectas: {quiz._incorrect_answers}")
+        
+if __name__ == "__main__":
+    run_quiz()
